43f8c26f5b9367d83bafb0c046617501d6adc2ae
unknown <blchilds55@gmail.com>
2020-10-21 18:35:35 -0500

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,11 @@
 var tasks = {};
 
+setInterval (function() {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  })
+}, 1800000);
+
 var createTask = function(taskText, taskDate, taskList) {
   // create elements that make up a task item
   var taskLi = $("<li>").addClass("list-group-item");
@@ -135,18 +141,20 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
 
-  // activate: function(event) {
-  //   console.log("activate",this);
-  // },
-  // deactivate: function(event) {
-  //   console.log("deactivate", this);
-  // },
-  // over: function(event) {
-  //   console.log("over", event.target);
-  // },
-  // out: function(event) {
-  //   console.log("out", event.target);
-  // },
+  activate: function(event) {
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
+  },
+  deactivate: function(event) {
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
+  },
+  over: function(event) {
+    $(event.target).addClass("dropover-active");
+  },
+  out: function(event) {
+    $(event.target).removeClass("dropover-active");
+  },
 
   update: function(event) {
     // array to store the task data in
@@ -179,12 +187,13 @@ $("#trash").droppable({
   drop: function(event, ui) {
     console.log("drop");
     ui.draggable.remove();
+    $(".bottom-trash").removeClass("bottom-trash-active");
   },
   over: function(event, ui) {
-    console.log("over");
+    $(".bottom-trash").addClass("bottom-trash-active");
   },
   out: function(event, ui) {
-    console.log("out");
+    $(".bottom-trash").removeClass("bottom-trash-active");
   }
 }) 
 
@@ -228,7 +237,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
